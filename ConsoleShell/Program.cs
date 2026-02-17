5c8f4a535c9149c2116bb35d820b761a3a5a8dbb
Ivan Oparin <ivanopa.rin@yandex.ru>
2026-02-17 13:45:54 +0300

    start project

---
+++ ConsoleShell/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleShell
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
